4d33959b8294d66684b2a6d8fe2118b1833772ad
Dante12129 <Blahduke31398ap>
2019-09-28 15:23:00 -0400

    Allow Specifying Port On Command Line

--- src/backend/Server.kt
+++ src/backend/Server.kt
@@ -6,9 +6,15 @@ class Server {
     private val api = Javalin.create()
 
     fun run(args: Array<String>) {
+        // Parse arguments
+        var port = 8080
+        if (args.isNotEmpty()) {
+            port = args[0].toInt()
+        }
+
         // Start server
-        println("Starting server...")
-        api.start(8080)
+        println("Starting server on port ${port}")
+        api.start(port)
 
         api.get("/") { ctx -> ctx.result("You have ridden into the DangerZone.") }
     }
