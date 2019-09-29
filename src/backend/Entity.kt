4eb9dcbe3c82fc0c14ea9787470e40093fc68659
Dante12129 <Blahduke31398ap>
2019-09-28 22:41:18 -0400

    Add Notification To Entity

--- src/backend/Entity.kt
+++ src/backend/Entity.kt
@@ -4,6 +4,10 @@ class Entity(val id: Int, var owningZone: Zone) {
     val fullId: String
         get() = ("$owningZone:$id")
 
+    fun notify(sender: Entity, message: String) {
+        println("${sender.toJson()} says $message")
+    }
+
     fun toJson(): String {
         return "{id: $id, zoneID: ${owningZone.id}}"
     }
